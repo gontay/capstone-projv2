bab079411971f30caa7b78c080bbb4ae0f9fa6f4
gontay <gtkw98@outlook.com>
2024-03-25 16:38:16 +0800

    deploy fix

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,12 +1,13 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendVerificationEmail  = async(
     email: string,
     token: string
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: "onboarding@gontay.xyz",
@@ -20,10 +21,10 @@ export const sendPassworResetEmail  = async(
     email: string,
     token: string
 ) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+    const resetLink = `${domain}/auth/new-password?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@gontay.xyz",
+        from: "auth@gontay.xyz",
         to: email,
         subject: "Password Reset Link",
         html: `<p> Click <a href="${resetLink}">here</a> to reset your password </p>`
